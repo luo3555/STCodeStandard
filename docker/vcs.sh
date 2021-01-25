8c3210eff56c2d82aefbb260955a4b20ea462df6
Cloyi <support@cloyi.com>
2021-01-25 21:57:43 +0800

    Modify vcs.sh and docker start command

--- docker/vcs.sh
+++ docker/vcs.sh
@@ -14,33 +14,37 @@ if [ 0 != `phpcs -n $1 | grep 'ERROR' | wc -l` ]; then
 fi
 }
 
-for line in $GITLIST
-do
-    if [ -f "$BASEDIR$line" ]; then
-        FILES=$FILES' '$BASEDIR$line
-    fi
-    if [ -d "$BASEDIR$line" ]; then
-        validateCodeStandard $DIRS$BASEDIR$line
-    fi
-done
+if [ "$GITLIST" ]; then
+    for line in $GITLIST
+    do
+        if [ -f "$BASEDIR$line" ]; then
+            FILES=$FILES' '$BASEDIR$line
+        fi
+        if [ -d "$BASEDIR$line" ]; then
+            validateCodeStandard $DIRS$BASEDIR$line
+        fi
+    done
 
-validateCodeStandard $FILES
+    validateCodeStandard $FILES
 
-if [ "$FLAG" = "access" ]; then
-    git status
-    echo '=================================================================================='
-    echo "\033[31mShow different?\033[0m  \033[32;40m[y/n]\033[0m"
-    read -p ':' input
-    if [ $input = 'y' ]; then
-        git diff
-    fi
-    echo "\033[31mAdd all files and dirs to GIT remotes? if YES will do [$(pwd)/git add . && git push]\033[0m  \033[32;40m[y/n]\033[0m"
-    read -p ":" inputpush
-    if [ $inputpush = 'y' ]; then
-        # do git push
-        git add .
-        git push origin
+    if [ "$FLAG" = "access" ]; then
+        git status
+        echo '=================================================================================='
+        echo "\033[31mShow different?\033[0m  \033[32;40m[y/n]\033[0m"
+        read -p ':' input
+        if [ $input = 'y' ]; then
+            git diff
+        fi
+        echo "\033[31mAdd all files and dirs to GIT remotes? if YES will do [$(pwd)/git add . && git push]\033[0m  \033[32;40m[y/n]\033[0m"
+        read -p ":" inputpush
+        if [ $inputpush = 'y' ]; then
+            # do git push
+            git add .
+            git push origin
+        fi
+    else
+        echo "\033[31mYou should fix all errors and try again. current nothing be pushed.\033[0m"
     fi
 else
-    echo "\033[31mYou should fix all errors and try again. current nothing be pushed.\033[0m"
+    git status
 fi
