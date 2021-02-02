ed3ec3ca5da7c3932d2c74aebc647eb854322566
luo3555 <luo3555@mail.com>
2021-02-02 10:15:53 +0800

    stop use git push, only validate

--- docker/vcs.sh
+++ docker/vcs.sh
@@ -27,26 +27,26 @@ if [ "$GITLIST" ]; then
 
     validateCodeStandard $FILES
 
-    if [ "$FLAG" = "access" ]; then
-        git status
-        echo '=================================================================================='
-        echo "\033[31mShow different?\033[0m  \033[32;40m[y/n]\033[0m"
-        read -p ':' input
-        if [ $input = 'y' ]; then
-            git diff
-        fi
-        echo "\033[31mAdd all files and dirs to GIT remotes? if YES will do [$(pwd)/git add . && git commit -m msg && git push]\033[0m  \033[32;40m[y/n]\033[0m"
-        read -p ":" inputpush
-        if [ $inputpush = 'y' ]; then
-            # do git push
-            git add .
-            read -p 'Commit message:' msg
-            git commit -m "$msg"
-            git push origin
-        fi
-    else
-        echo "\033[31mYou should fix all errors and try again. current nothing be pushed.\033[0m"
-    fi
+#    if [ "$FLAG" = "access" ]; then
+#        git status
+#        echo '=================================================================================='
+#        echo "\033[31mShow different?\033[0m  \033[32;40m[y/n]\033[0m"
+#        read -p ':' input
+#        if [ $input = 'y' ]; then
+#            git diff
+#        fi
+#        echo "\033[31mAdd all files and dirs to GIT remotes? if YES will do [$(pwd)/git add . && git commit -m msg && git push]\033[0m  \033[32;40m[y/n]\033[0m"
+#        read -p ":" inputpush
+#        if [ $inputpush = 'y' ]; then
+#            # do git push
+#            git add .
+#            read -p 'Commit message:' msg
+#            git commit -m "$msg"
+#            git push origin
+#        fi
+#    else
+#        echo "\033[31mYou should fix all errors and try again. current nothing be pushed.\033[0m"
+#    fi
 else
     git status
 fi
